683960b3505a965d8439f412524ccc9dbe6eba29
davidpassalia06 <dpassalia474@gmail.com>
2026-03-07 15:48:41 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,30 @@
+const C = 'lib-v4';
+const EXCLUDE = /googleapis|firebaseio|fonts\.g|openlibrary|covers\.openlibrary/;
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(C).then(c => c.addAll(['./', './index.html'])).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== C).map(k => caches.delete(k)))
+    ).then(() => clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (EXCLUDE.test(e.request.url)) return;
+  e.respondWith(
+    caches.match(e.request).then(r =>
+      r || fetch(e.request).then(res => {
+        return caches.open(C).then(c => {
+          c.put(e.request, res.clone());
+          return res;
+        });
+      }).catch(() => caches.match('./index.html'))
+    )
+  );
+});
